b06cfa43402be78b7d32fdc1e58d0ac812deb68c
Nasmer Fontanilla <nasmerfontanilla@gmail.com>
2025-06-14 00:15:51 +0800

    added some nativewindui folders and files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,8 @@
 // app/_layout.tsx
 import { Slot } from "expo-router";
+import { useInitialAndroidBarSync } from '../lib/useColorScheme';
 
 export default function RootLayout() {
+  useInitialAndroidBarSync();
   return <Slot />;
 }
